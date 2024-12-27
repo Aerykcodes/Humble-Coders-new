6e1dab91f2f4ab375ca070c22573ee63243b6de1
Nishesh <nsingla3_be23@thapar.edu>
2024-12-27 19:13:25 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Content from "./assets/pages/Content/Content"; // Ensure correct path
 import Home from "./assets/pages/Home/Home";
 import About from "./assets/pages/About/About";
 import Registration from "./assets/pages/Registration/Registration";
@@ -11,8 +10,7 @@ function App() {
     <div id="app-root">
       <BrowserRouter>
         <Routes>
-          <Route path="/content" element={<Content />} /> {/* Content Page */}
-          <Route path="/" element={<Content />} />
+          {/* <Route path="/content" element={<Content />} /> {/* Content Page */}
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/registration" element={<Registration />} />
